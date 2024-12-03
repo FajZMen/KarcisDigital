bd2010be16241d23db3923750d5dd69b849f9136
FajZ <fajarzman52@gmail.com>
2024-12-03 23:40:43 +0700

    Update History.py

--- Data/History.py
+++ Data/History.py
@@ -5,17 +5,18 @@ if 'karcisstrip' not in st.session_state:
     st.session_state['karcisstrip'] = []
 
 def gethistory(karcisid, jenis, platno, jammasuk, jamkeluar, totalharga):
+    if 'karcisstrip' not in st.session_state:
+        st.session_state['karcisstrip'] = []
     st.session_state['karcisstrip'].append([karcisid, jenis, platno, jammasuk, jamkeluar, totalharga])
 
 def historypage():
-    if st.session_state.karcisstrip:
-        historyframe = pd.DataFrame(st.session_state.karcisstrip, columns=["ID Karcis", "Jenis Kendaraan", "Plat Nomor", "Jam Masuk", "Jam Keluar", "Total Biaya"])
+    if 'karcisstrip' in st.session_state and st.session_state['karcisstrip']:
+        historyframe = pd.DataFrame(st.session_state['karcisstrip'], columns=["ID Karcis", "Jenis Kendaraan", "Plat Nomor", "Jam Masuk", "Jam Keluar", "Total Biaya"])
         st.dataframe(historyframe)
     else:
         st.write("Belum ada karcis yang tertulis.")
     
     if st.button("Hapus Histori"):
         st.write("Click lagi untuk menghapus atau pindah Halaman dan balik lagi")
-        st.session_state['karcisstrip'].clear()
-        st.rerun
-    
+        st.session_state['karcisstrip'] = []
+        st.rerun()
